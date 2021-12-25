6ec30e192422f8e48490d3e5c261311040802b11
princegobah <babynamek@gmail.com>
2021-12-25 16:45:11 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", () => {let form=document.querySelector('form');form.addEventListener('submit', (e)=>{e.preventDefault()
+  yoyo(e.target.new_task_description.value)
+  yoyo(e.target.user_name.value)
+ form.reset()
+})})
+function yoyo(hey){console.log(hey)
+let p=document.createElement('p')
+let btn=document.createElement('button')
+btn.addEventListener('click',deleter)
+btn.textContent='x'
+p.textContent=`${hey} `
+p.appendChild(btn) 
+document.querySelector("#list").appendChild(p)
+console.log(p)
+}
+
+function deleter(e){e.target.parentNode.remove()}
